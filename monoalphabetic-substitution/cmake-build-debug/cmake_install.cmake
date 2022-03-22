4217b28cec651000a8fc017dbbb1298a3b35461e
eslam gad <eslam.gad20502050@gmail.com>
2022-03-22 20:34:57 +0200

    final commit

--- monoalphabetic-substitution/cmake-build-debug/cmake_install.cmake
+++ monoalphabetic-substitution/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/eslam/Downloads/Faculty/crypto/first_alogrithm 
+# Install script for directory: /home/eslam/Downloads/Faculty/crypto/first alogrithm /monoalphabetic-substitution
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/eslam/Downloads/Faculty/crypto/first_alogrithm /cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/eslam/Downloads/Faculty/crypto/first alogrithm /monoalphabetic-substitution/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
