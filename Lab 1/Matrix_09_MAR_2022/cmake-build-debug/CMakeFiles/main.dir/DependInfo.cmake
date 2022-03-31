81677372b9f99d93eb949e24d755980085b52cb1
eslam gad <eslam.gad20502050@gmail.com>
2022-03-31 18:19:17 +0200

    first code

---
+++ Lab 1/Matrix_09_MAR_2022/cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/eslam/Downloads/Faculty/LAB/data structure/Lab 1/Matrix_09_MAR_2022/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/eslam/Downloads/Faculty/LAB/data structure/Lab 1/Matrix_09_MAR_2022/mat_test.cpp" "CMakeFiles/main.dir/mat_test.cpp.o" "gcc" "CMakeFiles/main.dir/mat_test.cpp.o.d"
+  "/home/eslam/Downloads/Faculty/LAB/data structure/Lab 1/Matrix_09_MAR_2022/matrix.cpp" "CMakeFiles/main.dir/matrix.cpp.o" "gcc" "CMakeFiles/main.dir/matrix.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Lab 1/Matrix_09_MAR_2022/matrix.cpp
+++ Lab 1/Matrix_09_MAR_2022/matrix.cpp
@@ -1,90 +1,124 @@
 #include "matrix.h"
+#include <random>
+#include "time.h"
 
+Matrix::Matrix(int rows_, int cols_) {
+    rows = rows_;
+    cols = cols_;
+    data = new float *[rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new float[cols];
+        for (int j = 0; j < cols; ++j) {
+            srandom(time(0));
+            data[i][j] = random();
+        }
+    }
+}
 
-Matrix::Matrix(int rows_, int cols_)
-{}
+Matrix::Matrix(int rows_, int cols_, float val) {
+    rows = rows_;
+    cols = cols_;
+    data = new float *[rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new float[cols];
+        for (int j = 0; j < cols; ++j) {
+            data[i][j] = val;
+        }
+    }
+}
 
-Matrix::Matrix(int rows_, int cols_, float val)
-{}
-
-Matrix::Matrix(int rows_, int cols_, MatrixType m_type)
-{}
+Matrix::Matrix(int rows_, int cols_, MatrixType m_type) {
+    rows = rows_;
+    cols = cols_;
+    data = new float *[rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new float[cols];
+        for (int j = 0; j < cols; ++j) {
+            if (m_type == random())
+                data[i][j] = random();
+            else if (m_type == Identity) {
+                if (i == j) {
+                    data[i][j] = 1;
+                } else {
+                    data[i][j] = 0;
+                }
+            }
+        }
+    }
+}
 
 /*Copy constructor: to prevent shallow copy*/
-Matrix::Matrix(const Matrix& m)
-{}
+Matrix::Matrix(const Matrix &m) {
+    rows = m.rows;
+    cols = m.cols;
+    data = new float *[rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new float[cols];
+        for (int j = 0; j < cols; ++j) {
+            data[i][j] = m.data[i][j];
+        }
+    }
+}
 
-Matrix::~Matrix()
-{}
+Matrix::~Matrix() {
+    for (int i = 0; i < rows; ++i) {
+        delete data[i];
+    }
+}
 
-int Matrix::get_rows() const
-{}
+int Matrix::get_rows() const {
+    return rows;
+}
 
-int Matrix::get_cols() const
-{}
+int Matrix::get_cols() const { return cols; }
 
-void Matrix::print() const
-{}
+void Matrix::print() const {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << data[i][j] << ' ';
+        }
+        cout << "\n";
+    }
+}
 
-float& Matrix::access(int r_ind, int c_ind)
-{}
+float &Matrix::access(int r_ind, int c_ind) {}
 
-Matrix Matrix::operator+(const Matrix& m) const
-{}
+Matrix Matrix::operator+(const Matrix &m) const {}
 
-Matrix Matrix::operator-(const Matrix& m) const
-{}
+Matrix Matrix::operator-(const Matrix &m) const {}
 
-Matrix Matrix::operator*(float val) const
-{}
+Matrix Matrix::operator*(float val) const {}
 
-Matrix Matrix::operator/(float val) const
-{}
+Matrix Matrix::operator/(float val) const {}
 
-Matrix Matrix::operator*(const Matrix& m) const
-{}
+Matrix Matrix::operator*(const Matrix &m) const {}
 
-Matrix Matrix::operator/(const Matrix& m) const
-{}
+Matrix Matrix::operator/(const Matrix &m) const {}
 
-bool Matrix::operator==(const Matrix& m) const
-{}
+bool Matrix::operator==(const Matrix &m) const {}
 
-bool Matrix::operator!=(const Matrix& m) const
-{}
+bool Matrix::operator!=(const Matrix &m) const {}
 
-bool Matrix::isIdentity() const
-{}
+bool Matrix::isIdentity() const {}
 
-bool Matrix::isIdempotent() const
-{}
+bool Matrix::isIdempotent() const {}
 
-bool Matrix::isSquare() const
-{}
+bool Matrix::isSquare() const {}
 
-bool Matrix::isSymmetric() const
-{}
+bool Matrix::isSymmetric() const {}
 
-bool Matrix::isUpperTriangle() const
-{}
+bool Matrix::isUpperTriangle() const {}
 
-bool Matrix::isLowerTriangle() const
-{}
+bool Matrix::isLowerTriangle() const {}
 
-Matrix Matrix::transpose() const
-{}
+Matrix Matrix::transpose() const {}
 
-void Matrix::fill(float val)
-{}
+void Matrix::fill(float val) {}
 
-float Matrix::minElement() const
-{}
+float Matrix::minElement() const {}
 
-float Matrix::maxElement() const
-{}
+float Matrix::maxElement() const {}
 
-float Matrix::determinant() const
-{}
+float Matrix::determinant() const {}
 
-string Matrix::toString() const
-{}
+string Matrix::toString() const {}
