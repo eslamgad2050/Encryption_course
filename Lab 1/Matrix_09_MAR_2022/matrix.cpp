3eca46741cde78ebda465cfd1b67401b11279f36
eslam gad <eslam.gad20502050@gmail.com>
2022-04-01 14:26:20 +0200

    -h

--- Lab 1/Matrix_09_MAR_2022/matrix.cpp
+++ Lab 1/Matrix_09_MAR_2022/matrix.cpp
@@ -1,15 +1,18 @@
+//by Eslam Gad
 #include "matrix.h"
 #include <random>
 #include "time.h"
+
+using namespace std;
 
 Matrix::Matrix(int rows_, int cols_) {
     rows = rows_;
     cols = cols_;
     data = new float *[rows];
+
     for (int i = 0; i < rows; ++i) {
         data[i] = new float[cols];
         for (int j = 0; j < cols; ++j) {
-            srandom(time(0));
             data[i][j] = random();
         }
     }
@@ -31,11 +34,11 @@ Matrix::Matrix(int rows_, int cols_, MatrixType m_type) {
     rows = rows_;
     cols = cols_;
     data = new float *[rows];
+    srandom(time(0));
     for (int i = 0; i < rows; ++i) {
         data[i] = new float[cols];
         for (int j = 0; j < cols; ++j) {
-            if (m_type == random())
-                data[i][j] = random();
+            if (m_type == Random) { data[i][j] = random(); }
             else if (m_type == Identity) {
                 if (i == j) {
                     data[i][j] = 1;
@@ -81,44 +84,229 @@ void Matrix::print() const {
     }
 }
 
-float &Matrix::access(int r_ind, int c_ind) {}
+float &Matrix::access(int r_ind, int c_ind) {
+    /*if (r_ind > rows || c_ind > cols) {
+       static float x = 0;
+        return x;
+    }*/
+    return *(&data[r_ind][c_ind]);
+}
 
-Matrix Matrix::operator+(const Matrix &m) const {}
+Matrix Matrix::operator+(const Matrix &m) const {
+    Matrix *result = new Matrix(rows, cols, 0);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result->data[i][j] = data[i][j] + m.data[i][j];
+        }
+    }
+    return *result;
+}
 
-Matrix Matrix::operator-(const Matrix &m) const {}
+Matrix Matrix::operator-(const Matrix &m) const {
+    Matrix *result = new Matrix(rows, cols, 0);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result->data[i][j] = data[i][j] - m.data[i][j];
+        }
+    }
+    return *result;
+}
 
-Matrix Matrix::operator*(float val) const {}
+Matrix Matrix::operator*(float val) const {
+    Matrix result(rows, cols, 0);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result.data[i][j] = data[i][j] * val;
+        }
+    }
+    return result;
+}
 
-Matrix Matrix::operator/(float val) const {}
+Matrix Matrix::operator/(float val) const {
+    Matrix *result = new Matrix(rows, cols, 0);
+    if (val == 0) {
+        return *result;
+    }
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result->data[i][j] = data[i][j] / val;
+        }
+    }
+    return *result;
+}
 
-Matrix Matrix::operator*(const Matrix &m) const {}
+Matrix Matrix::operator*(const Matrix &m) const {
+    Matrix *result = new Matrix(rows, m.cols, 0);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < m.cols; ++j) {
+            for (int k = 0; k < cols; ++k) {
+                result->data[i][j] += data[i][k] * m.data[k][j];
+            }
+        }
+    }
+    return *result;
+}
 
-Matrix Matrix::operator/(const Matrix &m) const {}
+Matrix Matrix::operator/(const Matrix &m) const {
+    Matrix *cofactors = new Matrix(rows, cols, 0);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            Matrix minor(rows - 1, cols - 1, 0);
+            for (int k = 0; k < rows - 1; ++k) {
+                for (int l = 0; l < cols - 1; ++l) {
+                    int r = k < i ? k : k + 1,
+                            c = l < j ? l : l + 1;
+                    minor.data[k][l] = data[r][c];
+                }
+            }
+            cofactors->data[i][j] = minor.determinant() * ((i + j) % 2 == 0 ? 1 : -1);
+        }
+    }
+    Matrix adjoint = cofactors->transpose(), result = (adjoint / m.determinant());
+    result.print();
+    return *this * result;
+}
 
-bool Matrix::operator==(const Matrix &m) const {}
+bool Matrix::operator==(const Matrix &m) const {
+    if (rows != m.rows || cols != m.cols) { return false; }
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (data[i][j] != m.data[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
-bool Matrix::operator!=(const Matrix &m) const {}
+bool Matrix::operator!=(const Matrix &m) const {
+    return !(*this == m);
+}
 
-bool Matrix::isIdentity() const {}
+bool Matrix::isIdentity() const {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if ((i == j && data[i][j] != 1) || (i != j && data[i][j] != 0))return false;
+        }
+    }
+    return true;
+}
 
-bool Matrix::isIdempotent() const {}
+bool Matrix::isIdempotent() const {
+    if (rows != cols) {
+        return false;
+    }
+    return (*this * *this) == *this;
+}
 
-bool Matrix::isSquare() const {}
+bool Matrix::isSquare() const {
+    if (cols == rows) { return true; }
+    else { return false; }
+}
 
-bool Matrix::isSymmetric() const {}
+bool Matrix::isSymmetric() const {
+    return *this == this->transpose();
+}
 
-bool Matrix::isUpperTriangle() const {}
+bool Matrix::isUpperTriangle() const {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (i > j && data[i][j] != 0) { return false; }
+        }
+    }
+    return true && isSquare();
+}
 
-bool Matrix::isLowerTriangle() const {}
 
-Matrix Matrix::transpose() const {}
+bool Matrix::isLowerTriangle() const {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (i < j && data[i][j] != 0) { return false; }
+        }
+    }
+    return true && isSquare();
+}
 
-void Matrix::fill(float val) {}
+Matrix Matrix::transpose() const {
+    Matrix *result = new Matrix(cols, rows);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result->data[j][i] = data[i][j];
+        }
+    }
+    return *result;
+}
 
-float Matrix::minElement() const {}
+void Matrix::fill(float val) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            data[i][j] = val;
+        }
+    }
+}
 
-float Matrix::maxElement() const {}
+float Matrix::minElement() const {
+    float max = MAXFLOAT;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (data[i][j] <
+                max) {
+                max = data[i][j];
+            }
+        }
+    }
+    return max;
+}
 
-float Matrix::determinant() const {}
+float Matrix::maxElement() const {
+    float max = MAXFLOAT * -1;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (data[i][j] > max) {
+                max = data[i][j];
+            }
+        }
+    }
+    return max;
+}
 
-string Matrix::toString() const {}
+float Matrix::determinant() const {
+    float result = 0;
+    if (rows != cols) {
+        return 0;
+    }
+    if (cols == 1) { return data[0][0]; }
+    else if (cols == 2) {
+        result += data[0][0] * data[1][1];
+        result -= data[0][1] * data[1][0];
+    } else {
+        for (int i = 0; i < rows; ++i) {
+            Matrix sub(rows - 1, cols - 1, 0);
+            for (int j = 0; j < rows - 1; ++j) {
+                for (int k = 0; k < cols - 1; ++k) {
+                    int temp = 0;
+                    if (k >= i) {
+                        temp = k + 1;
+                    } else {
+                        temp = k;
+                    }
+                    sub.data[j][k] = data[j + 1][temp];
+                }
+            }
+            result += data[0][i] * sub.determinant() * (i % 2 == 0 ? 1 : -1);
+        }
+    }
+}
+
+string Matrix::toString() const {
+    string result = "";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            ostringstream os;
+            os << data[i][j];
+            result += os.str() + " ";
+        }
+        result += "\n";
+    }
+    return result;
+}
--- Lab 1/Matrix_09_MAR_2022/matrix.h
+++ Lab 1/Matrix_09_MAR_2022/matrix.h
@@ -1,5 +1,6 @@
 #ifndef MATRIX_H
 #define MATRIX_H
+
 #include <iostream>
 #include <string>
 #include <cstdlib> // #include 	<stdlib.h>
@@ -8,8 +9,11 @@
 #include <cmath>
 
 using namespace std;
-enum MatrixType {Identity, Random};
-/* 
+enum MatrixType {
+    Identity, Random
+};
+
+/*
  * [1] Copy constructor (to prevent shallow copy)
  *
  * [2] Delegating Constructors:
@@ -35,45 +39,73 @@ enum MatrixType {Identity, Random};
  *  
 */
 
-class Matrix{
+class Matrix {
 private:
-	int rows;
-	int cols;
-	float **data;
+    int rows;
+    int cols;
+    float **data;
 public:
-	Matrix(int rows_, int cols_);
-	Matrix(int rows_, int cols_, float val);
-	Matrix(int rows_, int cols_, MatrixType m_type);
-	/*Copy constructor: to prevent shallow copy*/
-	Matrix(const Matrix& m);
-	~Matrix();
-	int get_rows() const;
-	int get_cols() const;
-	void print() const;
-	float& access(int r_ind, int c_ind);
-	Matrix operator+(const Matrix& m) const;
-	Matrix operator-(const Matrix& m) const;
-	Matrix operator*(float val) const;
-	Matrix operator/(float val) const;
-	Matrix operator*(const Matrix& m) const;
-	/*Bonus: Matrix Inverse*/
-	Matrix operator/(const Matrix& m) const;
-	bool operator==(const Matrix& m) const;
-	bool operator!=(const Matrix& m) const;
-	bool isIdentity() const;
-	bool isIdempotent() const;
-	bool isSquare() const;
-	bool isSymmetric() const;
-	bool isUpperTriangle() const;
-	bool isLowerTriangle() const;
-	Matrix transpose() const;
-	void fill(float val);
-	float minElement() const;
-	float maxElement() const;
-	/*Bonus: Determinant*/
-	float determinant() const;
-	/*Bonus: stringify Matrix*/
-	string toString() const;
+    Matrix(int rows_, int cols_);
+
+    Matrix(int rows_, int cols_, float val);
+
+    Matrix(int rows_, int cols_, MatrixType m_type);
+
+    /*Copy constructor: to prevent shallow copy*/
+    Matrix(const Matrix &m);
+
+    ~Matrix();
+
+    int get_rows() const;
+
+    int get_cols() const;
+
+    void print() const;
+
+    float &access(int r_ind, int c_ind);
+
+    Matrix operator+(const Matrix &m) const;
+
+    Matrix operator-(const Matrix &m) const;
+
+    Matrix operator*(float val) const;
+
+    Matrix operator/(float val) const;
+
+    Matrix operator*(const Matrix &m) const;
+
+    /*Bonus: Matrix Inverse*/
+    Matrix operator/(const Matrix &m) const;
+
+    bool operator==(const Matrix &m) const;
+
+    bool operator!=(const Matrix &m) const;
+
+    bool isIdentity() const;
+
+    bool isIdempotent() const;
+
+    bool isSquare() const;
+
+    bool isSymmetric() const;
+
+    bool isUpperTriangle() const;
+
+    bool isLowerTriangle() const;
+
+    Matrix transpose() const;
+
+    void fill(float val);
+
+    float minElement() const;
+
+    float maxElement() const;
+
+    /*Bonus: Determinant*/
+    float determinant() const;
+
+    /*Bonus: stringify Matrix*/
+    string toString() const;
 };
 
 #endif // MATRIX_H
