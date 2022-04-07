7ddea424d19d47207c5c2b3372faa4fb96780fc9
eslam gad <eslam.gad20502050@gmail.com>
2022-04-08 01:08:21 +0200

    expand des key(rndom task choose )

--- key_expansion/main.cpp
+++ key_expansion/main.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "enter seed" << std::endl;
+    int seed = 0;
+    cin >> seed;
+    srand(seed);
+    string name[] = {"\nGad", "\nRady", "\nAziz"};
+    int rand1 = -1, rand2 = -1;
+    rand1 = rand() % 3;
+    rand2 = rand() % 3;
+    while (rand2 == rand1) {
+        rand2 = rand() % 3;
+    }
+    cout << name[0] << " takes task number " << rand2 + 1;
+    cout << name[1] << " takes task number " << rand1 + 1;
+    cout << name[2] << " takes task number " << 4 - rand1 - rand2;
+
     return 0;
 
 }
